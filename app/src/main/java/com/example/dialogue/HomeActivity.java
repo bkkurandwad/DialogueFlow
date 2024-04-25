97efde0db01951ea59c397447e898f886e4f5456
bkkurandwad <bhargavkurandwad@gmail.com>
2024-04-25 13:07:18 +0530

    :wq

---
+++ app/src/main/java/com/example/dialogue/HomeActivity.java
@@ -0,0 +1,20 @@
+package com.example.dialogue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void gotochat(View view) {
+        startActivity(new Intent(HomeActivity.this, MainActivity.class));
+    }
+}
