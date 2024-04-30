6b5c2720050668ab5c30bdb1bf00f20b987598cc
bkkurandwad <bhargavkurandwad@gmail.com>
2024-04-30 23:58:44 +0530

    files added

--- app/src/main/java/com/example/dialogue/Edash.java
+++ app/src/main/java/com/example/dialogue/Edash.java
@@ -1,6 +1,11 @@
 package com.example.dialogue;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +13,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Edash extends AppCompatActivity {
 
+    private Button signout2;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +30,17 @@ public class Edash extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        signout2 = findViewById(R.id.sout2);
+
+        signout2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(Edash.this, "User Signed Out", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(Edash.this, Firstpage.class));
+            }
+        });
+
     }
 }
